70ed717c9d1e3a7a7ca6e3064d48498e76e8efc6
Scott Walters <me@scowalt.com>
2020-04-16 15:50:18 -0700

    Small worker refactor to try to fix heroku issue

--- src/worker.ts
+++ src/worker.ts
@@ -11,18 +11,19 @@ const workers: number = Number(process.env.WEB_CONCURRENCY);
 const MAX_JOBS_PER_WORKER: number = 1;
 
 function start() {
-	createIgnitionUpdateQueue().process(MAX_JOBS_PER_WORKER, async (job) => {
-		await IgnitionUpdater.update();
+	createIgnitionUpdateQueue().process(MAX_JOBS_PER_WORKER, (job) => {
+		return IgnitionUpdater.update();
 	});
 	createSpotifyUpdateQueue().process(MAX_JOBS_PER_WORKER, async (job) => {
 		const redirectUri: string = ""; // TODO need to figure out how to handle this in the spotify API
 		if (process.env.SPOTIFY_ACCOUNT_ACCESS_TOKEN === undefined ||
 			process.env.SPOTIFY_ACCOUNT_REFRESH_TOKEN === undefined) {
-			return assert(false, "Spotify access and refresh tokens must be set as environment variables");
+			assert(false, "Spotify access and refresh tokens must be set as environment variables");
+			return Promise.reject("Spotify access and refresh tokens must be set as environment variables");
 		}
 
-		await SpotifyUpdater.update(process.env.SPOTIFY_ACCOUNT_ACCESS_TOKEN, process.env.SPOTIFY_ACCOUNT_REFRESH_TOKEN, redirectUri);
+		return SpotifyUpdater.update(process.env.SPOTIFY_ACCOUNT_ACCESS_TOKEN, process.env.SPOTIFY_ACCOUNT_REFRESH_TOKEN, redirectUri);
 	});
 }
 
-throng({ workers, start, lifetime: Infinity });
+throng({ workers, start });
