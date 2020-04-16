0d5b8dc1a8c3abb4cf8631745633bad10b604568
Scott Walters <me@scowalt.com>
2020-04-16 16:31:36 -0700

    Trying another fix for heroku stalling queue issue

--- src/worker.ts
+++ src/worker.ts
@@ -8,13 +8,12 @@ import { SpotifyUpdater } from './jobs/SpotifyUpdater';
 import { IgnitionUpdater } from './jobs/IgnitionUpdater';
 
 const workers: number = Number(process.env.WEB_CONCURRENCY);
-const MAX_JOBS_PER_WORKER: number = 1;
 
 function start() {
-	createIgnitionUpdateQueue().process(MAX_JOBS_PER_WORKER, (job) => {
+	createIgnitionUpdateQueue().process((job) => {
 		return IgnitionUpdater.update();
 	});
-	createSpotifyUpdateQueue().process(MAX_JOBS_PER_WORKER, async (job) => {
+	createSpotifyUpdateQueue().process((job) => {
 		const redirectUri: string = ""; // TODO need to figure out how to handle this in the spotify API
 		if (process.env.SPOTIFY_ACCOUNT_ACCESS_TOKEN === undefined ||
 			process.env.SPOTIFY_ACCOUNT_REFRESH_TOKEN === undefined) {
