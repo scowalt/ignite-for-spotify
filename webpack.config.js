53d46a0be1f89618b73ef8d8816188f7d9a6f902
Scott Walters <me@scowalt.com>
2020-04-24 06:09:57 -0700

    Reindent js file to use tabs

--- webpack.config.js
+++ webpack.config.js
@@ -5,55 +5,55 @@ const TerserPlugin = require('terser-webpack-plugin');
 const path = require('path');
 
 module.exports = (env) => {
-    function buildNodeConfig(entry, output, dirname) {
-        return {
-            mode: 'development',
-            entry,
-            target: 'node',
-            devtool: 'source-map',
-            resolve: {
-                extensions: ['.ts', '.tsx', '.js', '.jsx']
-            },
-            module: {
-                rules: [
-                    { // Include TypeScript files in the TypeScript loader
-                        test: /\.ts(x?)$/,
-                        include: /src/,
-                        use: [{ loader: 'ts-loader' }]
-                    }
-                ]
-            },
-            output: {
-                path: __dirname + '/dist',
-                filename: output
-            },
-            node: {
-                // required for __dirname to properly resolve
-                // Also required for `bull` to work, see https://github.com/OptimalBits/bull/issues/811
+	function buildNodeConfig(entry, output, dirname) {
+		return {
+			mode: 'development',
+			entry,
+			target: 'node',
+			devtool: 'source-map',
+			resolve: {
+				extensions: ['.ts', '.tsx', '.js', '.jsx']
+			},
+			module: {
+				rules: [
+					{ // Include TypeScript files in the TypeScript loader
+						test: /\.ts(x?)$/,
+						include: /src/,
+						use: [{ loader: 'ts-loader' }]
+					}
+				]
+			},
+			output: {
+				path: __dirname + '/dist',
+				filename: output
+			},
+			node: {
+				// required for __dirname to properly resolve
+				// Also required for `bull` to work, see https://github.com/OptimalBits/bull/issues/811
 				__dirname: true, // required for __dirname to properly resolve
 			},
 			stats: 'errors-only', // lots of warning noise from sequelize
-            externals: ['pg', 'sqlite3', 'tedious', 'pg-hstore'], // Work-around for sequelize: https://github.com/sequelize/sequelize/issues/7509#issuecomment-616235834
-            plugins: [
+			externals: ['pg', 'sqlite3', 'tedious', 'pg-hstore'], // Work-around for sequelize: https://github.com/sequelize/sequelize/issues/7509#issuecomment-616235834
+			plugins: [
 				new webpack.DefinePlugin({ "global.GENTLY": false }), // https://github.com/node-formidable/formidable/issues/337#issuecomment-153408479
 
 			],
 			optimization: {
 				// Necessary to pack mysql2, which is required for sequelize. See https://github.com/sidorares/node-mysql2/issues/1016#issuecomment-549105591
 				minimizer: [
-				  new TerserPlugin({
-					terserOptions: {
-						keep_fnames: true
-					}
-				  })
+					new TerserPlugin({
+						terserOptions: {
+							keep_fnames: true
+						}
+					})
 				]
 			},
-        };
-    }
+		};
+	}
 
 	let configs = [
-        buildNodeConfig('./src/server.ts', 'server.js'),
-        buildNodeConfig('./src/worker.ts', 'worker.js'),
+		buildNodeConfig('./src/server.ts', 'server.js'),
+		buildNodeConfig('./src/worker.ts', 'worker.js'),
 		{
 			mode: 'development',
 			entry: './src/views/index.tsx',
@@ -71,12 +71,12 @@ module.exports = (env) => {
 				{
 					test: /\.s[ac]ss$/i,
 					use: [
-					// Creates `style` nodes from JS strings
-					'style-loader',
-					// Translates CSS into CommonJS
-					'css-loader',
-					// Compiles Sass to CSS
-					'sass-loader',
+						// Creates `style` nodes from JS strings
+						'style-loader',
+						// Translates CSS into CommonJS
+						'css-loader',
+						// Compiles Sass to CSS
+						'sass-loader',
 					],
 				},
 				{
