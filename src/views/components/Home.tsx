8a9df88840b8baa634fef6b7109a7b423916350a
Scott Walters <me@scowalt.com>
2020-04-24 11:17:30 -0700

    Fixed react bug, more work towards showing playlists

--- src/views/components/Home.tsx
+++ src/views/components/Home.tsx
@@ -1,7 +1,8 @@
 import React, { ReactNode } from "react";
+import { Playlists } from "./Playlists";
 
 export class Home extends React.Component<{}, {}> {
 	render(): ReactNode {
-		return <h1>Hello world!</h1>;
+		return <><h1>Hello world!</h1><Playlists></Playlists></>;
 	}
 }
---
+++ src/views/components/Playlists.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from "react";
+
+export class Playlists extends React.Component<{}, {}> {
+	constructor(props: React.Props<{}>) {
+		super(props);
+	}
+
+	render(): ReactNode {
+		return <>Playlists</>;
+	}
+}
