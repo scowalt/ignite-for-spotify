0cea744eccd477f79090fe42dd8e85603af03906
Scott Walters <me@scowalt.com>
2020-04-27 07:40:29 -0700

    Playlist pagination is now functional

--- src/views/components/SpotifySource.tsx
+++ src/views/components/SpotifySource.tsx
@@ -14,6 +14,7 @@ interface SpotifySourceState {
 	downloadAbort: AbortController;
 	readonly spotify: SpotifyWebApi.SpotifyWebApiJs;
 	playlists?: SpotifyApi.ListOfUsersPlaylistsResponse;
+	loading: boolean;
 }
 export class SpotifySource extends React.Component<SpotifySourceProps, SpotifySourceState> {
 	constructor(props: SpotifySourceProps) {
@@ -21,19 +22,26 @@ export class SpotifySource extends React.Component<SpotifySourceProps, SpotifySo
 		this.state = {
 			// TODO use this to cancel requests
 			downloadAbort: new AbortController(),
-			spotify: new SpotifyWebApi()
+			spotify: new SpotifyWebApi(),
+			loading: true
 		};
 		this.state.spotify.setAccessToken(props.auth.spotifyAccessToken);
 
-		this.getUserSpotifyPlaylists();
+		this.getUserSpotifyPlaylists(0);
 	}
 
 	componentWillUnmount(): void {
 		this.state.downloadAbort.abort();
 	}
 
-	getUserSpotifyPlaylists(): Promise<any> {
+	getUserSpotifyPlaylists(offset: number): Promise<any> {
+		if (!this.state.loading) {
+			this.setState(update(this.state, {
+				loading: {$set: true}
+			}));
+		}
 		return this.state.spotify.getUserPlaylists({
+			offset,
 			limit: PLAYLISTS_PER_REQUEST
 		}).then((value: SpotifyApi.ListOfUsersPlaylistsResponse) => {
 			// TODO handle multiple pages of playlists
@@ -51,11 +59,15 @@ export class SpotifySource extends React.Component<SpotifySourceProps, SpotifySo
 				}).then((response: string) => {
 					this.state.spotify.setAccessToken(response);
 					if (!this.state.downloadAbort.signal.aborted) {
-						return this.getUserSpotifyPlaylists();
+						return this.getUserSpotifyPlaylists(offset);
 					}
 				});
 			}
 			return Promise.reject(xhr);
+		}).finally(() => {
+			this.setState(update(this.state, {
+				loading: {$set: false}
+			}));
 		});
 	}
 
@@ -73,18 +85,26 @@ export class SpotifySource extends React.Component<SpotifySourceProps, SpotifySo
 			const paginators: ReactNode[] = [];
 			for (let index: number = 0; index < SpotifySource.getTotalPages(this.state.playlists); index ++) {
 				paginators.push(
-					<Pagination.Item key={index} active={index === SpotifySource.getCurrentPageNumber(this.state.playlists)}>
+					<Pagination.Item
+						key={index}
+						active={index === SpotifySource.getCurrentPageNumber(this.state.playlists)}
+						onClick={(): void => {this.getUserSpotifyPlaylists(index*PLAYLISTS_PER_REQUEST);}}>
 						{index+1}
 					</Pagination.Item>
 				);
 			}
+
+			let playlists: ReactNode;
+			if (this.state.loading) {
+				playlists = <>Loading</>;
+			} else {
+				playlists = this.state.playlists.items.map((playlist: SpotifyApi.PlaylistObjectSimplified, index: number) => {
+					return <ListGroup.Item key={index}>{playlist.name}</ListGroup.Item>;
+				});
+			}
 			return <Col>
 				<Row><ListGroup>
-					{
-						this.state.playlists.items.map((playlist: SpotifyApi.PlaylistObjectSimplified, index: number) => {
-							return <ListGroup.Item key={index}>{playlist.name}</ListGroup.Item>;
-						})
-					}
+					{ playlists }
 				</ListGroup></Row>
 				<Row>
 					<Pagination>{paginators}</Pagination>
