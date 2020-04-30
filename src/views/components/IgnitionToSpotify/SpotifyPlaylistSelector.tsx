984734a745a59550028bb756f03695e1de2d30cb
Scott Walters <me@scowalt.com>
2020-04-30 13:18:32 -0700

    Nested tabs working in spotify selection interface

--- src/views/components/IgnitionToSpotify/SpotifyPlaylistSelector.tsx
+++ src/views/components/IgnitionToSpotify/SpotifyPlaylistSelector.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from "react";
-import { Row, Col } from "react-bootstrap";
 import { SpotifyPlaylistListLoader } from "../shared/SpotifyPlaylistListLoader";
 import SpotifyWebApi from "spotify-web-api-js";
 import { SpotifyAuthInfo } from "../shared/SpotifyAuthInfo";
+import { FaSpotify } from 'react-icons/fa';
 
 interface Props extends React.Props<{}> {
 	auth: SpotifyAuthInfo;
@@ -26,14 +26,28 @@ export class SpotifyPlaylistSelector extends React.Component<Props, State> {
 	}
 
 	render(): ReactNode {
-		return <>
-			<Row><Col>Select an existing Spotify playlist...</Col></Row>
-			<Row><Col>
+		return <><ul className="nav nav-pills nav-fill mb-3" id="pills-tab" role="tablist">
+			<li className="nav-item">
+				<a className="nav-link active" id="pills-existing-tab" href="#pills-existing" data-toggle="pill" role="tab" aria-controls="pills-existing" aria-selected="true">
+					Select an existing <FaSpotify />Spotify playlist
+				</a>
+			</li>
+			<li className="nav-item">
+				<a className="nav-link" id="pills-new-tab" href="#pills-new" data-toggle="pill" role="tab" aria-controls="pills-new" aria-selected="false">
+					Create a new <FaSpotify />Spotify playlist
+				</a>
+			</li>
+		</ul>
+		<div className="tab-content" id="pills-tabContent">
+			<div className="tab-pane fade show active" id="pills-existing" role="tabpanel" aria-labelledby="pills-existing-tab">
 				<SpotifyPlaylistListLoader
 					spotify={this.state.spotify}
 					onPlaylistClicked={this.onPlaylistClicked.bind(this)}
 					playlistsPerRequest={5}/>
-			</Col></Row>
-		</>;
+			</div>
+			<div className="tab-pane fade" id="pills-new" role="tabpanel" aria-labelledby="pills-new-tab">
+				New
+			</div>
+		</div></>;
 	}
 }
