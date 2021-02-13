af73dda9ebb47db155fc957bcef38f5b54da68a8
Scott Walters <me@scowalt.com>
2021-02-13 12:06:02 -0800

    Fixed handleExpiredSpotifyToken

--- src/views/common/SpotifyHelpers.ts
+++ src/views/common/SpotifyHelpers.ts
@@ -1,6 +1,6 @@
-import SpotifyWebApi from "spotify-web-api-js";
+import {SpotifyWebApi} from "spotify-web-api-ts";
 
-export function handleExpiredSpotifyToken<T>(signal: AbortSignal, spotify: SpotifyWebApi.SpotifyWebApiJs, resume: () => Promise<T>): ((xhr: XMLHttpRequest) => Promise<T>) {
+export function handleExpiredSpotifyToken<T>(signal: AbortSignal, spotify: SpotifyWebApi, resume: () => Promise<T>): ((xhr: XMLHttpRequest) => Promise<T>) {
 	return async (xhr: XMLHttpRequest): Promise<any> => {
 		if (!signal.aborted && xhr.status === 401 && xhr.responseText.includes("The access token expired")) {
 			const response: Response = await fetch('/refreshSpotifyAuth', {
